1a7384933cd3b341156ee4925a2352ff08752100
zhukovgreen <zhukovgreen@ya.ru>
2019-02-22 16:06:25 +0100

    Black reformated

--- distributed_learning/src/01-warmup/basics.py
+++ distributed_learning/src/01-warmup/basics.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 # the C++ runtime of TensorFlow.
 
 # Run a graph with 1 constant operation.
-greeting = tf.constant('Hello, Prague!')
+greeting = tf.constant("Hello, Prague!")
 with tf.Session() as sess:
     result = sess.run(greeting)
     print(result)
@@ -20,10 +20,14 @@ a = tf.placeholder(tf.int16)
 b = tf.placeholder(tf.int16)
 add = tf.add(a, b)
 with tf.Session() as sess:
-    first_result = sess.run(add, feed_dict={a: 1, b: 2})
-    print('1 + 2 =', first_result)
-    second_result = sess.run(add, feed_dict={a: 3, b: 4})
-    print('3 + 4 =', second_result)
+    first_result = sess.run(
+        add, feed_dict={a: 1, b: 2}
+    )
+    print("1 + 2 =", first_result)
+    second_result = sess.run(
+        add, feed_dict={a: 3, b: 4}
+    )
+    print("3 + 4 =", second_result)
 
 # It might feel strange that the operations weren't specifically
 # assigned to a specific "graph", you just typed methods
@@ -31,7 +35,9 @@ with tf.Session() as sess:
 # Reason for this is that TensorFlow will create a "default" graph for
 # all unassigned operations.
 g = tf.get_default_graph()
-print(g.get_operations())  # => [1x const, 2x placeholder, 1x add] operations
+print(
+    g.get_operations()
+)  # => [1x const, 2x placeholder, 1x add] operations
 # So even though these two groups of operations (subgraphs)
 # have nothing connecting them, they belong into a same graph.
 
@@ -40,16 +46,22 @@ print(g.get_operations())  # => [1x const, 2x placeholder, 1x add] operations
 # resources and make sure different operations are used for e.g. dropout.
 graph_a = tf.Graph()
 with graph_a.as_default():
-    constant_in_a = tf.constant('constant in graph a')
+    constant_in_a = tf.constant("constant in graph a")
 
 graph_b = tf.Graph()
 with graph_b.as_default():
-    constant_in_b = tf.constant('constant in graph b')
+    constant_in_b = tf.constant("constant in graph b")
 
 with tf.Session(graph=graph_a) as sess:
-    print(sess.run(constant_in_a))  # => 'constant in graph a'
+    print(
+        sess.run(constant_in_a)
+    )  # => 'constant in graph a'
 
 with tf.Session(graph=graph_b) as sess:
-    print(sess.run(constant_in_b))  # => 'constant in graph b'
+    print(
+        sess.run(constant_in_b)
+    )  # => 'constant in graph b'
 
-print(g.get_operations())  # => default graph still only has the 4 originals
+print(
+    g.get_operations()
+)  # => default graph still only has the 4 originals
--- distributed_learning/src/06-ring/train.py
+++ distributed_learning/src/06-ring/train.py
@@ -19,38 +19,67 @@ is usually that simple.
 def neural_network(feature, target, mode):
     # Convert the target to a one-hot tensor of shape (batch_size, 10) and
     # with a on-value of 1 for each one-hot vector of length 10.
-    target = tf.one_hot(tf.cast(target, tf.int32), 10, 1, 0)
+    target = tf.one_hot(
+        tf.cast(target, tf.int32), 10, 1, 0
+    )
 
     # Reshape feature to 4d tensor with 2nd and 3rd dimensions being
     # image width and height final dimension being the number of color channels.
     feature = tf.reshape(feature, [-1, 28, 28, 1])
 
     # First conv layer will compute 32 features for each 5x5 patch
-    with tf.variable_scope('conv_layer1'):
+    with tf.variable_scope("conv_layer1"):
         h_conv1 = layers.conv2d(
-            feature, 32, kernel_size=[5, 5], activation_fn=tf.nn.relu)
+            feature,
+            32,
+            kernel_size=[5, 5],
+            activation_fn=tf.nn.relu,
+        )
         h_pool1 = tf.nn.max_pool(
-            h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+            h_conv1,
+            ksize=[1, 2, 2, 1],
+            strides=[1, 2, 2, 1],
+            padding="SAME",
+        )
 
     # Second conv layer will compute 64 features for each 5x5 patch.
-    with tf.variable_scope('conv_layer2'):
+    with tf.variable_scope("conv_layer2"):
         h_conv2 = layers.conv2d(
-            h_pool1, 64, kernel_size=[5, 5], activation_fn=tf.nn.relu)
+            h_pool1,
+            64,
+            kernel_size=[5, 5],
+            activation_fn=tf.nn.relu,
+        )
         h_pool2 = tf.nn.max_pool(
-            h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+            h_conv2,
+            ksize=[1, 2, 2, 1],
+            strides=[1, 2, 2, 1],
+            padding="SAME",
+        )
         # reshape tensor into a batch of vectors
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+        h_pool2_flat = tf.reshape(
+            h_pool2, [-1, 7 * 7 * 64]
+        )
 
     # Densely connected layer with 1024 neurons.
     h_fc1 = layers.dropout(
-        layers.fully_connected(h_pool2_flat, 1024, activation_fn=tf.nn.relu),
+        layers.fully_connected(
+            h_pool2_flat,
+            1024,
+            activation_fn=tf.nn.relu,
+        ),
         keep_prob=0.5,
-        is_training=mode == tf.contrib.learn.ModeKeys.TRAIN
+        is_training=mode
+        == tf.contrib.learn.ModeKeys.TRAIN,
     )
 
     # Compute logits (1 per class) and compute loss.
-    logits = layers.fully_connected(h_fc1, 10, activation_fn=None)
-    loss = tf.losses.softmax_cross_entropy(target, logits)
+    logits = layers.fully_connected(
+        h_fc1, 10, activation_fn=None
+    )
+    loss = tf.losses.softmax_cross_entropy(
+        target, logits
+    )
 
     return tf.argmax(logits, 1), loss
 
@@ -59,13 +88,21 @@ def main(_):
     # !! Initialize Horovod. !! #
     # hvd.init()
 
-    mnist = learn.datasets.mnist.read_data_sets('/src/datasets/')
+    mnist = learn.datasets.mnist.read_data_sets(
+        "/src/datasets/"
+    )
 
-    with tf.name_scope('input'):
-        image = tf.placeholder(tf.float32, [None, 784], name='image')
-        label = tf.placeholder(tf.float32, [None], name='label')
+    with tf.name_scope("input"):
+        image = tf.placeholder(
+            tf.float32, [None, 784], name="image"
+        )
+        label = tf.placeholder(
+            tf.float32, [None], name="label"
+        )
 
-    predict, loss = neural_network(image, label, tf.contrib.learn.ModeKeys.TRAIN)
+    predict, loss = neural_network(
+        image, label, tf.contrib.learn.ModeKeys.TRAIN
+    )
 
     opt = tf.train.RMSPropOptimizer(0.01)
 
@@ -73,13 +110,21 @@ def main(_):
     # opt = hvd.DistributedOptimizer(opt)
 
     global_step = tf.train.get_or_create_global_step()
-    train_op = opt.minimize(loss, global_step=global_step)
+    train_op = opt.minimize(
+        loss, global_step=global_step
+    )
 
     # !! BroadcastGlobalVariablesHook broadcasts variables from rank 0 to all others during init. !! #
     hooks = [
         # hvd.BroadcastGlobalVariablesHook(0),
         tf.train.StopAtStepHook(last_step=100),
-        tf.train.LoggingTensorHook(tensors={'step': global_step, 'loss': loss}, every_n_iter=10),
+        tf.train.LoggingTensorHook(
+            tensors={
+                "step": global_step,
+                "loss": loss,
+            },
+            every_n_iter=10,
+        ),
     ]
 
     # !! Limit which GPUs should this process consider using. !! #
@@ -87,11 +132,24 @@ def main(_):
     config.gpu_options.allow_growth = True
     # config.gpu_options.visible_device_list = str(hvd.local_rank())
 
-    with tf.train.SingularMonitoredSession(hooks=hooks, config=config) as sess:
-        assert type(opt) == horovod.tensorflow.DistributedOptimizer
+    with tf.train.SingularMonitoredSession(
+        hooks=hooks, config=config
+    ) as sess:
+        assert (
+            type(opt)
+            == horovod.tensorflow.DistributedOptimizer
+        )
         while not sess.should_stop():
-            image_, label_ = mnist.train.next_batch(100)
-            sess.run(train_op, feed_dict={image: image_, label: label_})
+            image_, label_ = mnist.train.next_batch(
+                100
+            )
+            sess.run(
+                train_op,
+                feed_dict={
+                    image: image_,
+                    label: label_,
+                },
+            )
 
 
 if __name__ == "__main__":
